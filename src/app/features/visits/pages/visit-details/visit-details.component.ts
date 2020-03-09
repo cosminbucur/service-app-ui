566888419f5197ca19d83ba3530aa2280a1317c8
cosmin_bucur <cosmindanbucur@gmail.com>
2020-03-09 21:36:09 +0200

    create visit details form and actions

--- src/app/features/visits/pages/visit-details/visit-details.component.ts
+++ src/app/features/visits/pages/visit-details/visit-details.component.ts
@@ -1,5 +1,9 @@
+import { NotificationService } from './../../../../core/services/notification.service';
+import { appRoutesNames } from './../../../../app.routes.names';
 import { Component, OnInit } from '@angular/core';
 import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-visit-details',
@@ -7,6 +11,19 @@ import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/dr
   styleUrls: ['./visit-details.component.scss']
 })
 export class VisitDetailsComponent implements OnInit {
+
+  public form: FormGroup;
+  public visit: any = {
+    id: null,
+    visitDate: new Date(),
+    firstName: null,
+    lastName: null,
+    company: null,
+    phone: null,
+    email: null,
+    licensePlate: null,
+    isActive: true
+  };
 
   onCarData = [
     'Front left - Summer',
@@ -27,15 +44,65 @@ export class VisitDetailsComponent implements OnInit {
       moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
     } else {
       transferArrayItem(event.previousContainer.data,
-                        event.container.data,
-                        event.previousIndex,
-                        event.currentIndex);
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex);
     }
   }
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private notificationService: NotificationService
+  ) { }
 
   ngOnInit() {
+    this.initializeData();
+  }
+
+  private async initializeData() {
+    this.createReactiveForm();
+  }
+
+  private createReactiveForm() {
+    this.form = new FormGroup({
+      visitDate:    new FormControl(this.visit.visitDate, Validators.required),
+      firstName:    new FormControl(this.visit.firstName, Validators.required),
+      lastName:     new FormControl(this.visit.lastName, Validators.required),
+      company:      new FormControl(this.visit.company, Validators.required),
+      phone:        new FormControl(this.visit.phone, Validators.pattern('\\d{10}')),
+      // TODO try Validators.email
+      email:        new FormControl(this.visit.email, Validators.pattern('^\\w+([\\.-]?\\w+)*@\\w+([\\.-]?\\w+)*(\\.\\w{2,3})+$')),
+      licensePlate: new FormControl(this.visit.licensePlate, Validators.required)
+    });
+  }
+
+  private goToVisitsPage() {
+    this.router.navigate([`/${appRoutesNames.VISITS}`]);
+  }
+
+  public cancel() {
+    this.goToVisitsPage();
+  }
+
+  public async save() {
+    if (!this.form.valid) {
+      this.notificationService.error('There are errors on the experience fields!');
+      return;
+    }
+
+    this.notificationService.success('Visit details saved.');
+  }
+
+  public print() {
+    this.notificationService.info('Printing in progress.');
+  }
+
+  public toggleActive() {
+    this.notificationService.info('Customer activated');
+  }
+
+  get firstName(): any {
+    return this.form.get('firstName');
   }
 
 }
