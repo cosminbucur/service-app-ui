04022179dac4e2073fb2617a60d657b7ea21af03
cosmin_bucur <cosmindanbucur@gmail.com>
2020-03-12 19:35:26 +0200

    move charts to reports and refactor charts

--- src/app/features/reports/size-chart/size-chart.component.ts
+++ src/app/features/reports/size-chart/size-chart.component.ts
@@ -19,17 +19,29 @@ export class SizeChartComponent implements OnInit {
   }
   };
 
-  public colors = [
-      // 'rgba(255, 99, 132, 0.2)',
-      // 'rgba(54, 162, 235, 0.2)',
-      // 'rgba(255, 206, 86, 0.2)',
-      // 'rgba(75, 192, 192, 0.2)',
-      // 'rgba(153, 102, 255, 0.2)',
-      // 'rgba(255, 159, 64, 0.2)'
-      'blue'
+  public chartColors = [
+    {
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(255, 159, 64, 0.2)'
+      ],
+      borderColor: [
+        'rgba(255,99,132,1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(153, 102, 255, 1)',
+        'rgba(255, 159, 64, 1)'
+      ],
+      borderWidth: 2,
+    }
   ];
-  public chartLabels = ['P215/65R15 95H', 'P215/65R17 95H'];
-  public chartType = 'bar';
+  public chartLabels = ['215 / 65 R15', '215 / 65 R17'];
+  public chartType = 'horizontalBar';
   public chartLegend = false;
 
   public data = [
