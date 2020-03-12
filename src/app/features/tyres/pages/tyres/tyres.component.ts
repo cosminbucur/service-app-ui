d2641ee3196810fb8dc156f4a25ed506c8563931
cosmin_bucur <cosmindanbucur@gmail.com>
2020-03-12 18:56:47 +0200

    add tires by brand and size to reports

--- src/app/features/tyres/pages/tyres/tyres.component.ts
+++ src/app/features/tyres/pages/tyres/tyres.component.ts
@@ -7,17 +7,6 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TyresComponent implements OnInit {
 
-  public tyresBySize = [
-    { size: 'P215/65R17 95H', count: 4 },
-    { size: 'P215/65R15 95H', count: 8 }
-  ];
-
-  public tyresByBrand = [
-    { brand: 'Continental', count: 8 },
-    { brand: 'Pirelli', count: 6 },
-    { brand: 'Michelin', count: 4 }
-  ];
-
   constructor() { }
 
   ngOnInit() {
