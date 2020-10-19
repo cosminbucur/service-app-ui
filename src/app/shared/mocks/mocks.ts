423491cfa6a4a7735fc99302d280627001a9e468
cosmin_bucur <cosmindanbucur@gmail.com>
2020-10-19 16:01:40 +0300

    add customer visit model

--- src/app/shared/mocks/mocks.ts
+++ src/app/shared/mocks/mocks.ts
@@ -34,5 +34,39 @@ export class MockData {
         { id: null, size: '215 / 65 R15', rimType: 'alloy', tyreType: 'regular', brand: 'Michelin',  wearLevel: 'medium', season: 'winter'}
     ];
 
+    public customerVisitModel: {
+        customerDetails: {
+            id: null,
+            firstName: 'Alex',
+            lastName: 'Vasile',
+            company: 'Ibm',
+            phoneNumber: '0722 333 444',
+            email: 'alex@gmail.com',
+            licensePlate: 'B123BOS',
+            isActive: true
+        },
+        serviceDetails: {
+            visitDate: '2020-10-20',
+            mechanicId: 1,
+            servicesPerformed: 'schimb complet',
+            observations: 'e bine ca nu e rau'
+        },
+        tyreDetails: {
+            storagePoint: 'R1E1E1',
+            capsNo: 12,
+            onCarData: [
+                { id: null, size: '215 / 65 R15', rimType: 'alloy', tyreType: 'regular', brand: 'Continental',  wearLevel: 'medium', season: 'summer'},
+                { id: null, size: '215 / 65 R15', rimType: 'alloy', tyreType: 'regular', brand: 'Continental',  wearLevel: 'medium', season: 'summer'},
+                { id: null, size: '215 / 65 R15', rimType: 'alloy', tyreType: 'regular', brand: 'Continental',  wearLevel: 'medium', season: 'summer'},
+                { id: null, size: '215 / 65 R15', rimType: 'alloy', tyreType: 'regular', brand: 'Continental',  wearLevel: 'medium', season: 'summer'}
+            ],
+            inStorageData: [
+                { id: null, size: '215 / 65 R15', rimType: 'alloy', tyreType: 'regular', brand: 'Michelin',  wearLevel: 'medium', season: 'winter'},
+                { id: null, size: '215 / 65 R15', rimType: 'alloy', tyreType: 'regular', brand: 'Michelin',  wearLevel: 'medium', season: 'winter'},
+                { id: null, size: '215 / 65 R15', rimType: 'alloy', tyreType: 'regular', brand: 'Michelin',  wearLevel: 'medium', season: 'winter'},
+                { id: null, size: '215 / 65 R15', rimType: 'alloy', tyreType: 'regular', brand: 'Michelin',  wearLevel: 'medium', season: 'winter'}
+            ]
+        }
+    }
 }
 
