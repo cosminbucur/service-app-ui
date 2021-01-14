9072a1ffd3d02ad688c9405e87350096176f4a43
cosmin_bucur <cosmindanbucur@gmail.com>
2021-01-15 00:56:42 +0200

    add jenkinsfile

--- src/app/features/register/register.component.ts
+++ src/app/features/register/register.component.ts
@@ -1,18 +1,22 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
-import { ThemePalette } from '@angular/material/core';
-import { Router } from '@angular/router';
-import { appRoutesNames } from 'src/app/app.routes.names';
-import { NotificationService } from 'src/app/core/services/notification.service';
-import { AuthService } from 'src/app/core/auth/auth.service';
+import { Component, OnInit } from "@angular/core";
+import {
+  FormGroup,
+  FormControl,
+  Validators,
+  FormBuilder,
+} from "@angular/forms";
+import { ThemePalette } from "@angular/material/core";
+import { Router } from "@angular/router";
+import { appRoutesNames } from "src/app/app.routes.names";
+import { NotificationService } from "src/app/core/services/notification.service";
+import { AuthService } from "src/app/core/auth/auth.service";
 
 @Component({
-  selector: 'app-register',
-  templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  selector: "app-register",
+  templateUrl: "./register.component.html",
+  styleUrls: ["./register.component.scss"],
 })
 export class RegisterComponent implements OnInit {
-
   public form: FormGroup;
 
   // public user: any = {
@@ -27,25 +31,26 @@ export class RegisterComponent implements OnInit {
   // };
 
   public user: any = {
-    username: 'test',
-    password: 'pass',
-    email: 'test@gmail.com',
-    phone: '0722333444',
-    firstName: 'test',
-    lastName: 'test'
+    username: "test",
+    password: "pass",
+    email: "test@gmail.com",
+    phone: "0722333444",
+    firstName: "test",
+    lastName: "test",
   };
 
   public password: string;
 
-  public color: ThemePalette = 'accent';
+  public color: ThemePalette = "accent";
   public checked = false;
   public disabled = false;
 
   constructor(
+    private fb: FormBuilder,
     private router: Router,
     private authService: AuthService,
     private notificationService: NotificationService
-  ) { }
+  ) {}
 
   public ngOnInit(): void {
     this.initializeData();
@@ -53,12 +58,12 @@ export class RegisterComponent implements OnInit {
 
   public onSubmit() {
     if (!this.form.valid) {
-      this.notificationService.error('There are errors on the user fields!');
+      this.notificationService.error("There are errors on the user fields!");
       return;
     }
 
     this.authService.register(this.form.value);
-    this.notificationService.success('User details saved.');
+    this.notificationService.success("User details saved.");
     // this.goToHome();
   }
 
@@ -71,21 +76,24 @@ export class RegisterComponent implements OnInit {
   }
 
   private createReactiveForm() {
-    const emailPattern = '^\\w+([\\.-]?\\w+)*@\\w+([\\.-]?\\w+)*(\\.\\w{2,3})+$';
-    const phonePattern = '\\d{10}';
+    const emailPattern =
+      "^\\w+([\\.-]?\\w+)*@\\w+([\\.-]?\\w+)*(\\.\\w{2,3})+$";
+    const phonePattern = "\\d{10}";
 
-    this.form = new FormGroup({
-      username:     new FormControl(this.user.username, Validators.required),
-      password:     new FormControl(this.user.password, Validators.required),
-      email:        new FormControl(this.user.email, [Validators.pattern(emailPattern), Validators.required]),
-      phone:        new FormControl(this.user.phone, [Validators.pattern(phonePattern)]),
-      firstName:    new FormControl(this.user.firstName, Validators.required),
-      lastName:     new FormControl(this.user.lastName, Validators.required)
+    this.form = this.fb.group({
+      username: [this.user.username, Validators.required],
+      password: [this.user.password, Validators.required],
+      email: [
+        this.user.email,
+        [Validators.pattern(emailPattern), Validators.required],
+      ],
+      phone: [this.user.phone, [Validators.pattern(phonePattern)]],
+      firstName: [this.user.firstName, Validators.required],
+      lastName: [this.user.lastName, Validators.required],
     });
   }
 
   private goToHome() {
     this.router.navigate([`/${appRoutesNames.HOME}`]);
   }
-
 }
